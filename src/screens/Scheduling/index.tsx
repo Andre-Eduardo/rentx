8b80f9dabbe6e4107ede7d2b2be9e3c43b637ea9
Andre <andre.eduardo11@hotmail.com>
2022-05-26 15:29:22 -0300

    Interface de Agendamento

---
+++ src/screens/Scheduling/index.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { useTheme } from 'styled-components';
+import { StatusBar } from 'expo-status-bar';
+import { BackButton } from '../../components/BackButton';
+
+import ArrowSvg from '../../assets/arrow.svg';
+
+import {
+  Container,
+  Header,
+  Title,
+  RentalPeriod,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  Content,
+  Footer,
+} from './styles';
+
+import { Button } from '../../components/Button';
+
+export function Scheduling() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <StatusBar style="light" translucent backgroundColor="transparent" />
+      <Header>
+        <BackButton
+          color={theme.colors.shape}
+          onPress={() => { }}
+        />
+
+        <Title>
+          Escolha uma
+          {'\n'}
+          data de inicio e
+          {'\n'}
+          fim do aluguel
+        </Title>
+        <RentalPeriod>
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue selected>26/05/2022</DateValue>
+          </DateInfo>
+
+          <ArrowSvg />
+
+          <DateInfo>
+            <DateTitle>ATE</DateTitle>
+            <DateValue selected={false} />
+          </DateInfo>
+        </RentalPeriod>
+      </Header>
+
+      <Content />
+
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
