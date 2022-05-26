a2d967eb384c475e105d088844e23cc1b5d38f5e
Andre <andre.eduardo11@hotmail.com>
2022-05-25 22:15:29 -0300

    Componente de Slider

---
+++ src/components/ImageSlider/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import {
+  Container,
+  ImageIndices,
+  ImageIndex,
+  CarImage,
+  CarImageWrapper,
+} from './styles';
+
+interface Props {
+  imagesUrl: string[];
+}
+
+export function ImageSlider({ imagesUrl }: Props) {
+  return (
+    <Container>
+      <ImageIndices>
+        <ImageIndex active />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+        <ImageIndex active={false} />
+      </ImageIndices>
+
+      <CarImageWrapper>
+        <CarImage
+          source={{ uri: imagesUrl[0] }}
+          resizeMode="contain"
+        />
+      </CarImageWrapper>
+    </Container>
+  );
+}
