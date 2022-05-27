d22e450df7d25be387891f8bea0cfbf2034423e7
Andre <andre.eduardo11@hotmail.com>
2022-05-26 21:53:55 -0300

    Interface de Detalhes do Agendamento

--- src/components/Accessory/styles.ts
+++ src/components/Accessory/styles.ts
@@ -2,13 +2,13 @@ import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
 
 export const Container = styled.View`
-  width: ${RFValue(109)}px;
-  height:${RFValue(92)}px;
+  width: ${RFValue(102)}px;
+  height:${RFValue(82)}px;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background_primary};
-  padding: 16px;
-  margin-bottom: 8px;
+  padding: ${RFValue(16)}px;
+  margin-bottom:${RFValue(8)}px;
 `;
 
 export const Name = styled.Text`
---
+++ src/screens/SchedulingDetails/index.tsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import { Feather } from '@expo/vector-icons';
+import { useTheme } from 'styled-components';
+import { RFValue } from 'react-native-responsive-fontsize';
+import { Accessory } from '../../components/Accessory';
+import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
+
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  Accessories,
+  Footer,
+  RentalPeriod,
+  CalendarIcon,
+  DateInfo,
+  DateTitle,
+  DateValue,
+  RentalPrice,
+  RentalPriceLabel,
+  RentalPriceDetails,
+  RentalPriceQuota,
+  RentalPriceTotal,
+} from './styles';
+import { Button } from '../../components/Button';
+
+export function SchedulingDetails() {
+  const theme = useTheme();
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={() => { }} />
+      </Header>
+      <CarImages>
+        <ImageSlider imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']} />
+      </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao Dia</Period>
+            <Price>R$580</Price>
+          </Rent>
+        </Details>
+        <Accessories>
+          <Accessory name="380km" icon={SpeedSvg} />
+          <Accessory name="380km" icon={AccelerationSvg} />
+          <Accessory name="380km" icon={ForceSvg} />
+          <Accessory name="380km" icon={GasolineSvg} />
+          <Accessory name="380km" icon={SpeedSvg} />
+          <Accessory name="380km" icon={SpeedSvg} />
+        </Accessories>
+        <RentalPeriod>
+          <CalendarIcon>
+            <Feather
+              name="calendar"
+              size={RFValue(24)}
+              color={theme.colors.shape}
+            />
+          </CalendarIcon>
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2022</DateValue>
+          </DateInfo>
+
+          <Feather
+            name="chevron-right"
+            size={RFValue(10)}
+            color={theme.colors.text}
+          />
+
+          <DateInfo>
+            <DateTitle>DE</DateTitle>
+            <DateValue>18/06/2022</DateValue>
+          </DateInfo>
+        </RentalPeriod>
+        <RentalPrice>
+          <RentalPriceLabel>TOTAL</RentalPriceLabel>
+          <RentalPriceDetails>
+            <RentalPriceQuota>R$ 580 x3 diárias</RentalPriceQuota>
+            <RentalPriceTotal>R$ 2.900</RentalPriceTotal>
+          </RentalPriceDetails>
+        </RentalPrice>
+      </Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
+    </Container>
+  );
+}
