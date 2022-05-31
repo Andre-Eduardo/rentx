123c40906fcd68051f804818ce78e2716a39e1a3
Andre <andre.eduardo11@hotmail.com>
2022-05-31 10:08:14 -0300

    Flexibilizando Interfaces

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -4,7 +4,7 @@ import { Home } from '../screens/Home';
 import { CarDetails } from '../screens/CarDetails';
 import { Scheduling } from '../screens/Scheduling';
 import { SchedulingDetails } from '../screens/SchedulingDetails';
-import { SchedulingComplete } from '../screens/SchedulingComplete';
+import { Confirmation } from '../screens/Confirmation';
 import { MyCars } from '../screens/MyCars';
 import { SignIn } from '../screens/SignIn/index';
 
@@ -30,8 +30,8 @@ export function StackRoutes() {
         component={SchedulingDetails}
       />
       <Screen
-        name="SchedulingComplete"
-        component={SchedulingComplete}
+        name="Confirmation"
+        component={Confirmation}
       />
       <Screen
         name="MyCars"
