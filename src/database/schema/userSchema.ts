f830d573675e0b903fa2bfc516866cd9db79e074
Andre <andre.eduardo11@hotmail.com>
2022-06-01 11:44:07 -0300

    Estrutura da base de dados

---
+++ src/database/schema/userSchema.ts
@@ -0,0 +1,32 @@
+import { tableSchema } from '@nozbe/watermelondb/Schema';
+
+const userSchema = tableSchema({
+  name: 'users',
+  columns: [
+    {
+      name: 'user_id',
+      type: 'string',
+    },
+    {
+      name: 'name',
+      type: 'string',
+    },
+    {
+      name: 'email',
+      type: 'string',
+    },
+    {
+      name: 'drive_license',
+      type: 'string',
+    },
+    {
+      name: 'avatar',
+      type: 'string',
+    },
+    {
+      name: 'token',
+      type: 'string',
+    },
+  ],
+});
+export { userSchema };
