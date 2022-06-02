aa1e1228ac85c0eb665dc5a66fc210a8d1a06cb8
Andre <andre.eduardo11@hotmail.com>
2022-06-01 22:43:36 -0300

    Sessões do Cadastro

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -10,6 +10,7 @@ import { AppStackRoutes } from './app.stack.routes';
 import HomeSvg from '../assets/home.svg';
 import CarSvg from '../assets/car.svg';
 import PeopleSvg from '../assets/people.svg';
+import { Profile } from '../screens/Profile';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -51,7 +52,7 @@ export function AppTabRoutes() {
       />
       <Screen
         name="Profile"
-        component={Home}
+        component={Profile}
         options={{
           tabBarIcon: ({ color }) => (
             <PeopleSvg width={24} height={24} fill={color} />
