92fc8836d7717d846d4fab326c5a84e3b5688e34
Andre <andre.eduardo11@hotmail.com>
2022-06-03 10:37:49 -0300

    Mostrando que usuário está Offline

--- src/components/Accessory/styles.ts
+++ src/components/Accessory/styles.ts
@@ -1,19 +1,18 @@
-import styled from 'styled-components/native';
 import { RFValue } from 'react-native-responsive-fontsize';
+import styled from 'styled-components/native';
 
 export const Container = styled.View`
-  width: ${RFValue(102)}px;
-  height:${RFValue(82)}px;
+  width: 109px;
+  height: 92px;
   justify-content: center;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.background_primary};
-  padding: ${RFValue(16)}px;
-  margin-bottom:${RFValue(8)}px;
+  padding: 16px;
+  margin-bottom: 8px;
 `;
 
 export const Name = styled.Text`
   font-family: ${({ theme }) => theme.fonts.primary_500};
-  font-size: ${RFValue(13)}px;
   color: ${({ theme }) => theme.colors.text};
-  margin-top: 4px;
+  font-size: ${RFValue(13)}px;
 `;
